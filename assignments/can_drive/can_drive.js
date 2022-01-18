e03ad2b072f50fa65505ff4f6f9987d15870a7df
Shyamali Dolekar <shyamlidolekar@gmail.com>
2022-01-18 13:20:10 +0530

    Can_Drive Assignment Submission

--- assignments/can_drive/can_drive.js
+++ assignments/can_drive/can_drive.js
@@ -1,5 +1,14 @@
 function CanDrive(hasDrivingLiscence, isTired, isSober) {
     // Write you code here
+    if (hasDrivingLiscence==true && isTired == false && isSober==true){
+        return "You can drive";
+    }
+    else if(hasDrivingLiscence==true && (isTired == true || isSober==false)){
+        return "You shouldn't drive";
+    }
+    else{
+        return "You cannot drive";
+    }
 }
 
 module.exports = CanDrive;
