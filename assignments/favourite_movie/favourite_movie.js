f9449163d5c4a104f8ea0370974cad13a697c816
Shyamali Dolekar <shyamlidolekar@gmail.com>
2022-01-20 14:11:35 +0530

    JS Assignment4 Submission

--- assignments/favourite_movie/favourite_movie.js
+++ assignments/favourite_movie/favourite_movie.js
@@ -2,6 +2,13 @@ const movies = [];
 
 function favouriteMovie(operation, movie) {
     // Write your code here
+    if (operation=="add"){
+        movies.push(movie);
+        // return movies;
+    }else if(operation == "remove"){
+        movies.pop();
+        // return movies;
+    }
+    return movies;
 }
-
 module.exports = favouriteMovie;
