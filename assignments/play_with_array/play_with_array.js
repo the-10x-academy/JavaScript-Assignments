22b43d95709d3e502937500da72f39ad3c0a87b8
Shyamali Dolekar <shyamlidolekar@gmail.com>
2022-01-23 23:14:48 +0530

    Play_with_Array Submission

--- assignments/play_with_array/play_with_array.js
+++ assignments/play_with_array/play_with_array.js
@@ -1,33 +1,24 @@
 function getEven(arr) {
-    /* 
-        Return an array containing only even numbers as a result
-        Example
-            Input: [1,4,5,77,8,90]
-            Output: [4,8,90]
-
-        Write you code below
-    */
-
+    var res = [];
+    for(var i = 0; i < arr.length; i++){
+       if (arr[i]%2===0){
+           res.push(arr[i]);
+       }
+   }
+    return res
 }
 
 function multiplyByN(arr, n) {
-    /* 
-        Return an array whose elements are multiplied by n
-        Example
-            Input: [1,3,4,55], 3
-            Output: [3,9,13,165]
-        Write you code below
-    */
-}
+    var res = [];
+    for(var i = 0; i < arr.length; i++){
+        res.push(arr[i]*n)
+    }
+    return res;
 
+}
 function removeNthElement(arr, n) {
-    /*
-        Return the array with the element present at index n removed
-        Example
-            Input: [1,3,4,6,7] 3
-            Output: [1,3,4,7]
-        Write you code below
-    */
+   arr.splice(n,1)
+   return arr;
 }
 
 module.exports = {
