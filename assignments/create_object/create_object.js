9d5b70505916fd8af5e2563c4bab04928d8b1b55
abhinav823 <ap661746@gmail.com>
2022-01-25 14:18:45 +0530

    assignment 6 commit

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,13 @@
 function CreateObject(arr) {
-    // Write your code here
+
+    obj={}
+    for (var i=0;i<arr.length;i=i+2){
+        var key=arr[i]
+        var value=arr[i+1];
+        obj[key]=value;
+    }
+    return obj
+
 }
 
 module.exports = CreateObject;
