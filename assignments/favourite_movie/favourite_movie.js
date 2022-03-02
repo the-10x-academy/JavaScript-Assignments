98d2ed82c0b4baafc7224e2f8e2b55fcaae8e597
kpravallika65 <kpravallika65@gmail.com>
2022-03-02 15:11:56 +0530

    Favourite Movie

--- assignments/favourite_movie/favourite_movie.js
+++ assignments/favourite_movie/favourite_movie.js
@@ -2,6 +2,14 @@ const movies = [];
 
 function favouriteMovie(operation, movie) {
     // Write your code here
+    if(operation==="add"){
+    movies.push(movie)
+    return movies
+    }
+    if(operation==="remove"){
+        movies.pop()
+        return movies
+    }
 }
 
 module.exports = favouriteMovie;
