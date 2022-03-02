42757e4f983e68525c047cbd5e5abe1b0524483d
suhail khazir bhat <suhailbhat7@gmail.com>
2022-03-02 22:27:06 +0530

    BMI

--- assignments/bmi_calculator/bmi_calculator.js
+++ assignments/bmi_calculator/bmi_calculator.js
@@ -1,6 +1,19 @@
 // This function should return the BMI for a person 
 function BMICalculator(mass, height) {
-    // Write your code here
-}
 
+    // Write your code here
+    //  if ((mass>0)||(height!=0))
+    if ((mass>0)&&(height>0))
+     {
+           BMI=mass/(height*height)
+           return BMI
+     }
+     else
+     {
+         return "INVALID INPUT"
+     }
+}
+// mass=parseInt(readLine())
+// height=parseInt(readLine())
+// console.log(BMICalculator(mass, height))
 module.exports = BMICalculator;
