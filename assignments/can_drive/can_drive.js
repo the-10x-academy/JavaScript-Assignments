3e30b247bb49c304ba32a2b7b6313ee16b27d9af
lalatendupany <lalatendupany.2000@gmail.com>
2022-03-03 15:21:15 +0530

    completed

--- assignments/can_drive/can_drive.js
+++ assignments/can_drive/can_drive.js
@@ -1,5 +1,20 @@
 function CanDrive(hasDrivingLiscence, isTired, isSober) {
     // Write you code here
+    if(hasDrivingLiscence){
+        if(isTired==false && isSober==true){
+            return "You can drive"
+        }
+        else if(isTired==true){
+            return "You shouldn't drive"
+        }
+        else{
+            return "You shouldn't drive"
+        }
+        
+    }
+    else{
+        return "You cannot drive"
+    }
 }
 
 module.exports = CanDrive;
