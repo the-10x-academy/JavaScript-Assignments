e552b42cffc6655869bcd558f7f3f0a370bcdd92
Infa-K <kevinjanc17@gmail.com>
2022-03-05 09:00:09 +0530

    first commit of favourite_movie

--- assignments/favourite_movie/favourite_movie.js
+++ assignments/favourite_movie/favourite_movie.js
@@ -2,6 +2,21 @@ const movies = [];
 
 function favouriteMovie(operation, movie) {
     // Write your code here
+    function arrayRemove(arr, value) { 
+    
+        return arr.filter(function(ele){ 
+            return ele != value; 
+        });
+    }
+
+    if(operation==="add"){ 
+    movies.push(movie)
+    return movies 
+    }
+    if(operation==="remove"){
+        m = arrayRemove(movies, movie)
+        return m
+    }
 }
 
 module.exports = favouriteMovie;
