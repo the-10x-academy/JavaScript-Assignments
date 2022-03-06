b1634cf55e192e2eae2bb30b4545d7935e42c452
Infa-K <kevinjanc17@gmail.com>
2022-03-06 15:08:38 +0530

    First commit of Create_Object

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,13 @@
 function CreateObject(arr) {
     // Write your code here
+    const obj = {};
+
+    for (let i = 0; i < arr.length - 1; i+=2)
+    {
+        obj[arr[i]] = arr[i+1]; 
+    }
+
+    return obj;
 }
 
 module.exports = CreateObject;
