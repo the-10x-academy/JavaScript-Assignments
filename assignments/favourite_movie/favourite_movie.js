7b8a18974602e8f161c3163a7452211c20a87e20
sahil <tiwari.sahil@aol.com>
2022-03-12 18:54:16 +0530

    Solution-2

--- assignments/favourite_movie/favourite_movie.js
+++ assignments/favourite_movie/favourite_movie.js
@@ -2,9 +2,11 @@ const movies = [];
 
 function favouriteMovie(operation, movie) {
     if (operation == "add"){
+
         movies.push(movie);}
     else{
         movies.pop(movie)}
     return movies
+
 }
 module.exports = favouriteMovie;
