76052d25bbba784cfdc320e722608194d6b32d7c
sahil <tiwari.sahil@aol.com>
2022-03-15 13:49:35 +0530

    run_callback sol

--- assignments/run_callback/run_callback.js
+++ assignments/run_callback/run_callback.js
@@ -1,6 +1,7 @@
 function RunCallback(a, b, cb) {
     // Write you code here, you need to add a and b, pass the result into callback function cb as argument return the result
 let add = a + b;
+
 return cb(add)}
 
 module.exports = RunCallback;
