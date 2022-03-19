ec1d3d1f018c9e0c94d6da115cd211856ec848b3
asur <asur@192.168.1.9>
2022-03-19 22:03:54 +0530

    Favorite Movie

--- assignments/favourite_movie/favourite_movie.js
+++ assignments/favourite_movie/favourite_movie.js
@@ -2,6 +2,13 @@ const movies = [];
 
 function favouriteMovie(operation, movie) {
     // Write your code here
+    if (operation === 'add'){
+        movies.push(movie)
+    }
+    else{
+        movies.pop()
+    }
+    return movies
 }
 
 module.exports = favouriteMovie;
