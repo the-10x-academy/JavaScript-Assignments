f03d03e4c7be87babcd67074d008dc083d4ba4b5
asur <asur@192.168.1.9>
2022-03-19 21:56:04 +0530

    CanDrive Completed

--- assignments/can_drive/can_drive.js
+++ assignments/can_drive/can_drive.js
@@ -1,5 +1,11 @@
 function CanDrive(hasDrivingLiscence, isTired, isSober) {
     // Write you code here
+    if(hasDrivingLiscence && !isTired && isSober)
+        return "You can drive";
+    else if(!hasDrivingLiscence)
+        return "You cannot drive";
+    else
+        return "You shouldn't drive";
 }
 
 module.exports = CanDrive;
